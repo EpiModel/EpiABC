5a9256e8077012a758eb535d956dbff51007142c
Samuel Jenness <smjenness@gmail.com>
2019-01-13 17:52:51 -0500

    Convert package naming to EpiABC

---
+++ R/EpiABC-package.R
@@ -0,0 +1,23 @@
+
+#' EpiABC: performing efficient approximate Bayesian computation sampling
+#' schemes using R
+#'
+#' Description
+#'
+#' \tabular{ll}{ Package: EpiABC Type: Package Version: 1.5 Date: 2015-06-30
+#' License: GPL-3 LazyLoad: yes }
+#'
+#' @name EpiABC-package
+#' @aliases EpiABC-package EpiABC
+#' @docType package
+#' @author Franck Jabot, Thierry Faure, Nicolas Dumoulin, Carlo Albert
+#' @seealso \code{\link{ABC_rejection}}, \code{\link{ABC_sequential}},
+#' \code{\link{ABC_mcmc}}, \code{\link{SABC}}, \code{\link{binary_model}},
+#' \code{\link{binary_model_cluster}}
+#' @keywords package abc
+#'
+#' @import MASS mnormt parallel pls abc lhs tensorA
+#' @importFrom stats as.formula cov cov.wt lm optimize rnorm runif sd uniroot var
+#' @importFrom utils flush.console read.table write.table
+#'
+NULL
